fce5c4b500f968bd0a31ecf6f645656de932707c
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 16:19:13 +0000

    ex how to use class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+// import Header from './components/Header';
+
+// function App() {
+
+//   return (
+
+//     //all should be insde dev
+//     <div className="container">
+//       <Header />
+//     </div>
+//   );
+// }
+
+//for class based components:
+
+import React, { Component } from 'react';
 import Header from './components/Header';
 
-function App() {
-
-  return (
-
-    //all should be insde dev
-    <div className="container">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (<h1>Hello from class</h1>)
+  }
 }
 
 export default App;
