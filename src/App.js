33ab9ba309ddda5f940e7e89669b95c8cda18356
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 16:09:14 +0000

    can write js directly and make it dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 function App() {
+
+  const name = 'Mahsa';
+
+  //conditional:
+  const x = true;
+
   return (
 
     //all should be insde dev
-    <div className="App">
+    <div className="container">
       <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+      {/* Ternary operator: */}
+      <h3>Hello {x ? 'Yes' : 'No'}</h3>
     </div>
   );
 }
