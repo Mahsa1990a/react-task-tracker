728f9a2b3e8236af41d4723f379a986b42b6badd
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 16:35:43 +0000

    propTypes to help our code more robust

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+//will help your code more robust
+import PropTypes from 'prop-types'
+
 const Header = (props) => {
   return (
     <header>
@@ -9,6 +12,10 @@ const Header = (props) => {
 //or we don't need to pass it to Header in App, instead we do:
 Header.defaultProps = {
   title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired, //we can change the type be string, number, ...
 }
 
 export default Header;
