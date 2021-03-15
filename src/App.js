b1e74cf8c0c18cba6099fc5cbf9551a72aab6e07
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 16:26:31 +0000

    added props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-// import Header from './components/Header';
-
-// function App() {
-
-//   return (
-
-//     //all should be insde dev
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// }
-
-//for class based components:
-
-import React, { Component } from 'react';
 import Header from './components/Header';
 
-class App extends Component {
-  render() {
-    return (<h1>Hello from class</h1>)
-  }
+function App() {
+
+  return (
+
+    //all should be insde dev
+    <div className="container">
+      <Header title='Hello' />
+    </div>
+  );
 }
 
 export default App;
+
+
+// Class based Components:
+
+// import React, { Component } from 'react';
+// import Header from './components/Header';
+
+// class App extends Component {
+//   render() {
+//     return (<h1>Hello from class</h1>)
+//   }
+// }
+
+// export default App;
