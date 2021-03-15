c296eff94e5209ed74a25b21d6a49aa866f77ca3
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 20:04:02 +0000

    added icon zarbdar

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,11 @@
+import { FaTimes } from 'react-icons/fa' //fa:font awsome , FaTimes: icon zarbdar
+
 const Task = ( { task }) => {
   return (
     <div className='task'>
-      <h3>{task.text}</h3>
+      <h3>
+        {task.text} <FaTimes />
+      </h3>
       <p>{task.day}</p>
     </div>
   )
