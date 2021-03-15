00005b1cc97ba2467a2dee93e6375fee200558d4
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-15 19:49:40 +0000

    moved useState into App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+
+  const [tasks, setTasks] = useState( //as default it is same taske arr
+    [
+      {
+        id: 1,
+        text: "AAA",
+        day: "Feb 6th @1:30pm",
+        reminder: true 
+      },
+      {
+        id: 2,
+        text: "BBB",
+        day: "Dec 7th @2:30pm",
+        reminder: true 
+      },
+      {
+        id: 3,
+        text: "CCC",
+        day: "Jan 8th @3:30pm",
+        reminder: false 
+      }
+    ]
+  );
 
   return (
 
     //all should be insde dev
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,53 +1,8 @@
-// const tasks = [
-//   {
-//     id: 1,
-//     text: "AAA",
-//     day: "Feb 6th @1:30pm",
-//     reminder: true 
-//   },
-//   {
-//     id: 2,
-//     text: "BBB",
-//     day: "Dec 7th @2:30pm",
-//     reminder: true 
-//   },
-//   {
-//     id: 3,
-//     text: "CCC",
-//     day: "Jan 8th @3:30pm",
-//     reminder: false 
-//   }
-// ];
 
 //WE WANT tasks TO BE A PART OF COMPONENT, so we use useState
 
-import { useState } from 'react';
-
-const Tasks = () => {
-
-  //using useState:
-  const [tasks, setTasks] = useState( //as default it is same taske arr
-    [
-      {
-        id: 1,
-        text: "AAA",
-        day: "Feb 6th @1:30pm",
-        reminder: true 
-      },
-      {
-        id: 2,
-        text: "BBB",
-        day: "Dec 7th @2:30pm",
-        reminder: true 
-      },
-      {
-        id: 3,
-        text: "CCC",
-        day: "Jan 8th @3:30pm",
-        reminder: false 
-      }
-    ]
-  );
+const Tasks = ({ tasks }) => {
+  
   return (
     <>
       {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
