a57d7d585eb7f75f9644f268fbaf2b05b509dbbd
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-17 15:08:42 +0000

    added component level state for AddTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,4 +1,12 @@
+//Each input is gonna have it's own piece of state(component level state not App level state)
+import { useState } from 'react';
+
 const AddTask = () => {
+
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
   return (
     //it's gonna be a form so change div to form:
     <form className='add-form'>
