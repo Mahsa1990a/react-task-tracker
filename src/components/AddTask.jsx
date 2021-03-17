80ee1c4b803aaa113fe3abd61ee69ece37f8d585
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-17 14:12:29 +0000

    addtask form

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,13 @@
+const AddTask = () => {
+  return (
+    //it's gonna be a form so change div to form:
+    <form className='add-form'>
+      <div className='form-control'>
+
+      </div>
+      
+    </form>
+  )
+}
+
+export default AddTask
