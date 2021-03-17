979fcf2e428a556b3c4d064bf242bd7a91597cef
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-17 14:59:16 +0000

    added more in AddTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,9 +2,21 @@ const AddTask = () => {
   return (
     //it's gonna be a form so change div to form:
     <form className='add-form'>
-      <div className='form-control'>
 
+      <div className='form-control'>
+        <label>Task</label>
+        <input type='text' placeholder='Add Task'/>
       </div>
+      <div className='form-control'>
+        <label>Day & Time</label>
+        <input type='text' placeholder='Add Day & Time'/>
+      </div>
+      <div className='form-control'>
+        <label>Set Reminder</label>
+        <input type='checkbox'/>
+      </div>
+
+      <input type='submit' value='Save Task' />
       
     </form>
   )
