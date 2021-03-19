2840de78a5f7c0732ae09689408155075130b205
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-03-19 01:12:27 +0000

    refactor useEffect and use index instead of id

--- src/App.js
+++ src/App.js
@@ -14,16 +14,22 @@ function App() {
   const [tasks, setTasks] = useState([]); //we deleted obj and moved it to db.json to fetch data from there
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks');
-      const data = await res.json();
 
-      // console.log("data", data);
-      return data;
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
     }
-
-    fetchTasks()
+    getTasks()
   }, [])
+
+  //Fetch Tasks:
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    // console.log("data", data);
+    return data;
+  }
 
   //Add Task
   const addTask = (task) => {
